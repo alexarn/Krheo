a3a285e0373491877449dcd99d1768252e29920c
Alex <arnaudalex@hotmail.com>
2011-11-01 17:32:05 +0100

    Second commit

---
+++ Utils.pm
@@ -0,0 +1,54 @@
+package Krheo::Utils;
+use strict;
+use warnings;
+use Krheo::Object;
+use Data::Dumper;
+
+sub to_do {
+    my $file = shift;
+
+    my $todo;
+    my $lines = parse_infos_file($file);
+
+    my $ip = shift(@$lines);
+    my $hostname = shift(@$lines);
+    my $c_letter = shift(@$lines);
+
+    my $obj = Krheo::Object->new('host');
+    $obj->{attr} = [ 
+	{ key => "use", val => "windows-server" },
+	{ key => "host_name", val => $hostname },
+	{ key => "alias", val => $hostname },
+	{ key => "address", val => $ip },
+	{ key => "hostgroups", val => "windows-server" },
+    ];
+
+    push @{ $todo->{host_list} }, $obj;
+
+    foreach my $letter ( @$lines ) {
+	my $service = Krheo::Object->new('service');
+	$service->{attr} = [ 
+	    { key => "use", val => "generic-service" },
+	    { key => "host_name", val => $hostname },
+	    { key => "service_description", val  => "$letter:\\ Drive Space" },
+	    { key => "check_command", val => "check_nt!USEDDISKSPACE!-l " . lc($letter) . " -w 80 -c 90" },
+	];
+	push @{ $todo->{service_list} }, $service;
+    }
+    return $todo;
+}
+
+sub parse_infos_file {
+    my $file = shift;
+
+    my @lines;
+    open( FH, "<$file" )
+        || die "could not open $file for reading: $!";
+    while (<FH>) {
+	chomp;
+	$_ =~ s/\r//g;
+	push @lines, $_;
+    }
+    return \@lines;
+}
+1;
--- process.pl
+++ process.pl
@@ -3,16 +3,17 @@
 use strict;
 use warnings;
 use Krheo::File;
+use Krheo::Utils;
 use Symbol;
 use Data::Dumper;
 
-my $infos_file = "infos";
+my $infos_file = "/home/alex/nico/infos";
 my $tmpl = "/home/alex/nico/Krheo/tmpl/nagios.tmpl";
 my $config_file = "/home/alex/nico/windows.cfg";
 
-my $config = Krheo::File->in($config_file);
+my $todo = Krheo::Utils::to_do($infos_file);
 
-# work to do
+my $config = Krheo::File->in($config_file);
 
 open( TMPL, "<$tmpl" ) || die "could not open $tmpl for reading: $!";
 my $new;
@@ -23,6 +24,8 @@ while (<TMPL>) {
 foreach my $type_list ( keys %$config ) {
     my $content = "";
     $content .= $_->dmp for @{ $config->{$type_list} };
+    $content .= $_->dmp for @{ $todo->{$type_list} };
+
     $new =~ s/<<$type_list>>/$content/;
 }
 print $new;
